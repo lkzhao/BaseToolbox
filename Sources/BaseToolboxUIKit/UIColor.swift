6dcb883660c23b4d429cdd8381bbfbde6f9ea0b5
Luke Zhao <me@lkzhao.com>
2022-10-21 12:01:24 -0700

    optimize hexStringWithAlpha

--- Sources/BaseToolboxUIKit/UIColor.swift
+++ Sources/BaseToolboxUIKit/UIColor.swift
@@ -100,9 +100,13 @@ extension UIColor {
 
         getRed(&r, green: &g, blue: &b, alpha: &a)
 
-        let rgba: Int = (Int)(r * 255) << 24 | (Int)(g * 255) << 16 | (Int)(b * 255) << 8 | (Int)(a * 255) << 0
-
-        return String(format: "#%08x", rgba)
+        if a < 1.0 {
+            let rgb: Int = (Int)(r * 255) << 16 | (Int)(g * 255) << 8 | (Int)(b * 255) << 0
+            return String(format: "#%06x", rgb)
+        } else {
+            let rgba: Int = (Int)(r * 255) << 24 | (Int)(g * 255) << 16 | (Int)(b * 255) << 8 | (Int)(a * 255) << 0
+            return String(format: "#%08x", rgba)
+        }
     }
 
     public func lighter(amount: CGFloat = 0.2) -> UIColor {
