21b0d8a6001a40304b7f631cf8a233a945e5e95d
Luke Zhao <me@lkzhao.com>
2022-10-21 15:01:45 -0700

    fix hexStringWithAlpha

--- Sources/BaseToolboxUIKit/UIColor.swift
+++ Sources/BaseToolboxUIKit/UIColor.swift
@@ -101,11 +101,11 @@ extension UIColor {
         getRed(&r, green: &g, blue: &b, alpha: &a)
 
         if a < 1.0 {
-            let rgb: Int = (Int)(r * 255) << 16 | (Int)(g * 255) << 8 | (Int)(b * 255) << 0
-            return String(format: "#%06x", rgb)
-        } else {
             let rgba: Int = (Int)(r * 255) << 24 | (Int)(g * 255) << 16 | (Int)(b * 255) << 8 | (Int)(a * 255) << 0
             return String(format: "#%08x", rgba)
+        } else {
+            let rgb: Int = (Int)(r * 255) << 16 | (Int)(g * 255) << 8 | (Int)(b * 255) << 0
+            return String(format: "#%06x", rgb)
         }
     }
 
